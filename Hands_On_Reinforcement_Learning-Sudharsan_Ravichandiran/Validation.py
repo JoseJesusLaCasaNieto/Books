9d6d00c1cf3dee9b9c07c23e62c98104a931c3bb
José Jesús La Casa Nieto <148141178+JoseJesusLaCasaNieto@users.noreply.github.com>
2024-06-11 17:03:01 +0200

    Hands-On Reinforcement Learning - Validation

--- Hands_On_Reinforcement_Learning-Sudharsan_Ravichandiran/Validation.py
+++ Hands_On_Reinforcement_Learning-Sudharsan_Ravichandiran/Validation.py
@@ -1,6 +1,6 @@
 import gymnasium as gym
 
-env = gym.make("ALE/MsPacman-v5", render_mode="human")
+env = gym.make("CartPole-v1", render_mode="human")
 observation, info = env.reset(seed=42)
 for _ in range(1000):
    action = env.action_space.sample()  # this is where you would insert your policy
